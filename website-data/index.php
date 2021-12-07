12d151b921bedb1f0a33cd1aafa8e980821d1c95
Riszky Hermawan <71969296+riszkyhermawan@users.noreply.github.com>
2021-12-07 08:55:29 +0700

    Update index.php

--- website-data/index.php
+++ website-data/index.php
@@ -1,118 +1,151 @@
 <?php
-  include_once "header.php";
+include_once "header.php";
 ?>
 
 
-  <div class="container-fluid">
-    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-      <div class="carousel-indicators">
-        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-      </div>
-      <div class="carousel-inner">
-        <div class="carousel-item active">
-          <img src="source/slide1.png" class="d-block w-100" alt="slide1" height="600">
-        </div>
-        <div class="carousel-item">
-          <img src="source/slide2.png" class="d-block w-100" alt="slide2" height="600">
-        </div>
-        <div class="carousel-item">
-          <img src="source/slide3.jpg" class="d-block w-100" alt="slide3" height="600">
-        </div>
-      </div>
-      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-      </button>
-      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-      </button>
+<div class="container-fluid">
+  <div id="carouselExampleIndicators" class="carousel slide " data-bs-ride="carousel">
+    <div class="carousel-indicators">
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
     </div>
+    <div class="carousel-inner">
+      <div class="carousel-item active" data-bs-interval="10000">
+        <video autoplay muted loop class="d-block mx-auto img-fluid">
+          <source src="source/slide1.mp4" type="video/mp4">
+        </video>
+      </div>
+      <div class="carousel-item " data-bs-interval="5000">
+        <video autoplay muted loop class="d-block mx-auto img-fluid">
+          <source src="source/slide2.mp4" type="video/mp4">
+        </video>
+      </div>
+      <div class="carousel-item" data-bs-interval="10000">
+        <video autoplay muted loop class="d-block mx-auto img-fluid">
+          <source src="source/slide3.mp4" type="video/mp4">
+        </video>
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
   </div>
+</div>
+
+
+<div class="cn-bsl">
   <div class="container">
-    <div class="cn-2">
+    <div class="top-c">
       <h2 class="fw-bold">
-        Category
+        CATEGORY
       </h2>
-      <p class="text-center">Explore our wonderful categories, don't see what you looking for? Feel free to request</p>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos.</p>
     </div>
   </div>
   <div class="container-fluid">
     <div class="row align-items-center">
-      <div class="col">
-        <a href="#">
-          <div class="card c1 img-fluid mx-auto d-block" style="width: 18rem;">
-            <img src="source/anime.jpg" class="card-img-top" alt="anime">
-            <div class="card-body">
-              <h5 class="card-text text-center">Anime</h5>
+      <div class="col b1">
+        <div class="bsl">
+          <div class="bestseller img-fluid  bsl1 mx-auto d-block">
+            <div class="text">
+              <h2>
+                Anime
+              </h2>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque, maxime voluptate! Aliquid libero dolorum totam cum fugiat officiis natus nisi!</p>
             </div>
+            <span class="label"></span>
           </div>
-        </a>
+        </div>
       </div>
-      <div class="col">
-        <a href="#">
-          <div class="card c2 img-fluid mx-auto d-block" style="width: 18rem;">
-            <img src="source/drawing.jpg" class="card-img-top" alt="drawing">
-            <div class="card-body">
-              <h5 class="card-text text-center">Drawing</h5>
+      <div class="col b2">
+        <div class="bsl">
+          <div class="bestseller img-fluid  bsl2 mx-auto d-block">
+            <div class="text">
+              <h2>
+                Painting
+              </h2>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque, maxime voluptate! Aliquid libero dolorum totam cum fugiat officiis natus nisi!</p>
             </div>
+            <span class="label"></span>
           </div>
-        </a>
+        </div>
       </div>
-      <div class="col">
-        <a href="#">
-          <div class="card c1 img-fluid mx-auto d-block" style="width: 18rem;">
-            <img src="source/3d-character.png" class="card-img-top" alt="3d-character" height="190px">
-            <div class="card-body">
-              <h5 class="card-text text-center">3D Character</h5>
+      <div class="col b3">
+        <div class="bsl">
+          <div class="bestseller img-fluid  bsl3 mx-auto d-block">
+            <div class="text">
+              <h2>
+                Poster
+              </h2>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque, maxime voluptate! Aliquid libero dolorum totam cum fugiat officiis natus nisi!</p>
             </div>
+            <span class="label"></span>
           </div>
-        </a>
-      </div>
-      <div class="col">
-        <a href="#">
-          <div class="card c2 img-fluid mx-auto d-block" style="width: 18rem;">
-            <img src="source/painting.jpg" class="card-img-top" alt="painting" height="190px">
-            <div class="card-body">
-              <h5 class="card-text text-center">Painting</h5>
-            </div>
-          </div>
-        </a>
+        </div>
       </div>
     </div>
   </div>
-  <div class="container">
-    <div class="cn-3">
-      <h2 class="fw-bold">
-        Creator
-      </h2>
-      <p>Meet our creator, we are ready to make your awesome project</p>
-    </div>
+</div>
+<div class="container">
+  <div class="cn-3">
+    <h2 class="fw-bold">
+      Creator
+    </h2>
+    <p>Meet our creator, we are ready to make your awesome project</p>
   </div>
-  <div class="container-fluid">
-    <div class="row align-items-center creator">
-      <div class="col">
-        <a href="#">
-          <img src="source/Antonio.png" alt="Antonio">
-        </a>
-      </div>
-      <div class="col">
-        <a href="#">
-          <img src="source/Darius.png" alt="Darius">
-        </a>
-      </div>
-      <div class="col">
-        <a href="#">
-          <img src="source/Deren.png" alt="Deren">
-        </a>
-      </div>
-      <div class="col">
-        <a href="#">
-          <img src="source/Michelle.png" alt="Michelle">
-        </a>
-      </div>
+</div>
+<div class="cn-bsl container-fluid">
+  <div class="row align-items-center">
+    <div class="col b1">
+      <a href="artist.html">
+        <div class="container-fluid bsl">
+          <div class="bestseller img-fluid  bsl4 mx-auto d-block">
+            <div class="text">
+              <h2>
+                Jessica
+              </h2>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque, maxime voluptate! Aliquid libero dolorum totam cum fugiat officiis natus nisi!</p>
+            </div>
+            <span class="label"></span>
+          </div>
+        </div>
+      </a>
+    </div>
+    <div class="col b2">
+      <a href="artist.html">
+        <div class="container-fluid bsl">
+          <div class="bestseller img-fluid  bsl5 mx-auto d-block">
+            <div class="text">
+              <h2>
+                Wahyu
+              </h2>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque, maxime voluptate! Aliquid </p>
+            </div>
+            <span class="label"></span>
+          </div>
+        </div>
+      </a>
+    </div>
+    <div class="col b3">
+      <a href="artist.html">
+        <div class="container-fluid bsl">
+          <div class="bestseller img-fluid  bsl6 mx-auto d-block">
+            <div class="text">
+              <h2>
+                Billy
+              </h2>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque, maxime voluptate! </p>
+            </div>
+            <span class="label"></span>
+          </div>
+        </div>
+      </a>
     </div>
   </div>
   <div class="container">
@@ -128,10 +161,10 @@
       <div class="container">
         <div class="row align-items-center">
           <div class="col review">
-            <img src="source/Carrick.png" alt="Carrick">
+            <img src="https://i.pinimg.com/564x/f7/41/a8/f741a8e514f63e679d07b46b4faf67ba.jpg" alt="Carrick">
           </div>
           <div class="col text-start">
-            <img src="source/5-star.png" alt="5-star" class="float-start img-fluid" width="150">
+            <img src="source/5-star.png">
             <br>
             <br>
             <br>
@@ -148,7 +181,7 @@
   </div>
   <br>
   <br>
-  
+
   <?php
   include_once "footer.php";
-?>
+  ?>
