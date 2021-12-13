00cc18aeb869ca3626f79e818e73baa293bba47a
Riszky Hermawan <71969296+riszkyhermawan@users.noreply.github.com>
2021-12-13 23:14:08 +0700

    Update footer.php

--- website-data/footer.php
+++ website-data/footer.php
@@ -22,27 +22,31 @@
                 </div>
                 <div class="col-lg-3 col-md-6 text-md-end item social  pt-4 pb-2">
                     <a href="#" class="text-decoration-none">
-                      <img src="logo/wa.jpg" class="ms-6" style="width: 25px; height: 25px;">
+                        <img src="logo/wa.jpg" class="ms-6" style="width: 25px; height: 25px;">
                     </a>
                     <a href="#" class="text-decoration-none">
-                      <img src="logo/ig.jpg" class="ms-6" style="width: 25px; height: 25px">
+                        <img src="logo/ig.jpg" class="ms-6" style="width: 25px; height: 25px">
                     </a>
                     <a href="#" class="text-decoration-none">
-                      <img src="logo/link.jpg" class="ms-" style="width: 25px; height: 25px">
+                        <img src="logo/link.jpg" class="ms-" style="width: 25px; height: 25px">
                     </a>
-                </div>
                 </div>
             </div>
-            <div class="cpr">
-                <div class="container text-center">
-                <p class="text mb-0 py-2">Copyright © BIKINLAGI.ID</p>
-                </div>
-            </div>          
         </div>
-    </footer>
-  </div>
-  
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-  </body>
+        <div class="cpr">
+            <div class="container text-center">
+                <p class="text mb-0 py-2">Copyright © BIKINLAGI.ID</p>
+            </div>
+        </div>
+</div>
+</footer>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+<script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+<script src="review.js">
+</script>
+</body>
+
 </html>
