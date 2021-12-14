1440f324f1ae699e2b49146ab9cae4ac8e336bd7
Riszky Hermawan <71969296+riszkyhermawan@users.noreply.github.com>
2021-12-14 07:04:57 +0700

    Update header.php

--- website-data/header.php
+++ website-data/header.php
@@ -13,10 +13,11 @@
     <title>Bikinlagi ID</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="styles.css" type="text/css">
-    <link rel="stylesheet" href="bsl.css">
+    <link rel="stylesheet" href="bsls.css">
     <link rel="icon" href="logo/logo.png">
-    <link rel="stylesheet" href="review.css">
+    <link rel="stylesheet" href="reviews.css">
     <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
+    <link rel="stylesheet" href="loading.css">
 </head>
 <body>
   <header>
