6d5bb87e3cda09c2aa476778fa428ea9685c70ad
Riszky Hermawan <71969296+riszkyhermawan@users.noreply.github.com>
2021-12-14 07:06:29 +0700

    Update footer.php

--- website-data/footer.php
+++ website-data/footer.php
@@ -5,21 +5,6 @@
                 <div class="col-sm-4 col-md-3 item">
                     <img src="logo/logo.png">
                 </div>
-                <div class="col-sm-4 col-md-3 item">
-                    <h3>About</h3>
-                    <ul>
-                        <li><a href="#">Category</a></li>
-                        <li><a href="#">Creator</a></li>
-                        <li><a href="#">Gallery</a></li>
-                    </ul>
-                </div>
-                <div class="col-sm-4 col-md-3 item">
-                    <h3>Support</h3>
-                    <ul>
-                        <li><a href="#">Contact</a></li>
-                        <li><a href="#">Testimony</a></li>
-                    </ul>
-                </div>
                 <div class="col-lg-3 col-md-6 text-md-end item social  pt-4 pb-2">
                     <a href="#" class="text-decoration-none">
                         <img src="logo/wa.jpg" class="ms-6" style="width: 25px; height: 25px;">
@@ -45,8 +30,9 @@
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
 <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
-<script src="review.js">
-</script>
+<script src="review.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script src="loading.js"></script>
 </body>
 
 </html>
